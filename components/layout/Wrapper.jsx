8b7c2896658c657ab7bc6936e5c7767c1643878e
kjartanhr <kjartan@kjartan.io>
2023-07-15 22:02:04 +0000

    make the nav cuter

--- components/layout/Wrapper.jsx
+++ components/layout/Wrapper.jsx
@@ -12,19 +12,19 @@ export function Wrapper({children}) {
                 isOpen={mobileMenuIsOpen}
             />
             <div className="fixed top-0 left-0 w-full bg-white/[50%] backdrop-blur-sm border-b border-gray-200/[75%] z-[50]">
-                <div className="container">
+                <div className="container py-5">
                     <div className="flex gap-4 items-center">
                         <Link href="/" className="flex gap-2 items-center">
                             <img src="/vendor/logo.png" className="h-7 w-7" />
-                            <p className="text-lg font-medium py-5 md:py-0">bgp.guide</p>
+                            <p className="text-lg font-medium">bgp.guide</p>
                         </Link>
-                        <div className="hidden md:flex flex-grow justify-center nav">
+                        <div className="hidden md:flex flex-grow justify-center gap-3 nav">
                             <a href="#">Guides</a>
                             <a href="#">Resources</a>
                             <a href="#">Documentation</a>
                             <a href="#">Donate</a>
                         </div>
-                        <a href="#" className="hidden md:flex text-sm flex gap-2 items-center px-3 py-6 border-b border-transparent hover:border-black text-gray-700 hover:text-black">
+                        <a href="#" className="hidden md:flex gap-2 items-center text-sm py-1 px-3 border border-gray-200/[75%] rounded-full bg-white/[10%] backdrop-blur hover:bg-gray-300/[20%] transition duration-150">
                             GitHub
                             <SiGithub className="h-4 w-4" />
                         </a>
