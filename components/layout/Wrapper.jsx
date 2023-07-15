1493793832954e5bfa38ff7a4467fb98c2bb3f12
kjartanhr <kjartan@kjartan.io>
2023-07-15 19:15:28 +0000

    init wrapper + navbar

--- components/layout/Wrapper.jsx
+++ components/layout/Wrapper.jsx
@@ -1,5 +1,31 @@
+import { SiGithub } from "@icons-pack/react-simple-icons"
+
 export const Wrapper = ({children}) => (
-    <div>
-        
+    <div className="min-h-screen flex flex-col">
+        <div className="fixed top-0 left-0 w-full bg-white/[50%] backdrop-blur-sm border-b border-gray-200/[75%]">
+            <div className="container">
+                <div className="flex gap-4 items-center">
+                    <p className="text-lg font-medium">bgpguide</p>
+                    <div className="flex flex-grow justify-center nav">
+                        <a href="#">Guides</a>
+                        <a href="#">Resources</a>
+                        <a href="#">Documentation</a>
+                        <a href="#">Donate</a>
+                    </div>
+                    <a href="#" className="text-sm flex gap-2 items-center px-3 py-6 border-b border-transparent hover:border-black text-gray-700 hover:text-black">
+                        GitHub
+                        <SiGithub className="h-4 w-4" />
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div className="flex-grow">
+            {children}
+        </div>
+        <div className="bg-white">
+            <div className="container py-4">
+                <p className="text-xs text-gray-500">Copyleft bgp.guide contributors.</p>
+            </div>
+        </div>
     </div>
 )
